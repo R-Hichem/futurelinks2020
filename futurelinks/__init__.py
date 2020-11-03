aa1abe316c7aeb6e856e38eafd368c399e135ef6
ProjetCPI <57575684+ProjetCPI@users.noreply.github.com>
2020-11-03 19:04:52 +0100

    reverted everything

--- futurelinks/__init__.py
+++ futurelinks/__init__.py
@@ -11,9 +11,9 @@ app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 app.config['UPLOAD_FOLDER_CSV'] = UPLOAD_FOLDER_CSV
 app.config['SECRET_KEY'] = '3d6f45a5fc12445dbac2f59c3b6c7cb1'
 app.config['CORS_HEADERS'] = 'Content-Type'
-#app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
-connect_string = 'mysql+pymysql://b74d0f09dd1574:3c5f697a@eu-cdbr-west-03.cleardb.net/heroku_d0271aab022de6f'
-app.config['SQLALCHEMY_DATABASE_URI'] = connect_string 
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+#connect_string = 'mysql+pymysql://b74d0f09dd1574:3c5f697a@eu-cdbr-west-03.cleardb.net/heroku_d0271aab022de6f'
+#app.config['SQLALCHEMY_DATABASE_URI'] = connect_string 
 
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
